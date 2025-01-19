bbee0e3377a8afac3253db2b799ed7aafb618c91
Bruno Inzunza <bruno.inzunza24@gmail.com>
2025-01-19 16:45:06 +0100

    updated projects

--- web-portfolio/src/pages/index.tsx
+++ web-portfolio/src/pages/index.tsx
@@ -79,45 +79,38 @@ export default function Home() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <ProjectCard 
               title="Algorithmic Trading" 
-              description="Developed a sophisticated algorithmic trading system that utilizes technical analysis techniques to analyze market trends and execute trades automatically, resulting in a consistent increase versus S&P500."
-              technologies={['Python']} 
+              description="Designed and implemented an algorithmic trading system. Fully backtested with custom strategies."
+              technologies={['Python', 'Trading']} 
               githubLink="https://github.com/brinzunza/bat" 
               liveDemoLink="https://github.com/brinzunza/BAT/blob/main/meanReversion.ipynb" 
             />
             <ProjectCard 
-              title="MERN Blog" 
-              description="Created a full-stack blog application using the MERN stack (MongoDB, Express, React, Node.js), featuring user authentication, real-time posts, and a responsive design that enhances user engagement."
-              technologies={['MongoDB', 'Express', 'React', 'Node.js']} 
+              title="PracticeDebugging.com" 
+              description="Developed a web-based platform that allows developers to practice and enhance debugging skills by solving simulated problems."
+              technologies={['React', 'Next', 'MongoDB']} 
               githubLink="https://github.com/brinzunza" 
               liveDemoLink="https://github.com/brinzunza" 
             />
             <ProjectCard 
-              title="LeetCode Clone" 
-              description="Built a clone of the popular coding platform LeetCode, allowing users to practice coding problems, track their progress, and practice debugging issues, all while providing a seamless user experience."
-              technologies={['React', 'Firebase', 'Node.js']} 
-              githubLink="https://github.com/brinzunza" 
-              liveDemoLink="https://github.com/brinzunza" 
+              title="LaLiga Analysis and Predictor" 
+              description="Built a sports analysis tool using web scraping to gather and aggreagate raw data on LaLiga teams and match statistics."
+              technologies={['Python', 'Web-Scraping', 'Data Science']} 
+              githubLink="https://github.com/brinzunza/laLigaML" 
+              liveDemoLink="https://github.com/brinzunza/laLigaML" 
             />
             <ProjectCard 
-              title="Speed Typing Test" 
-              description="Designed an interactive speed typing test application that challenges users to improve their typing speed and accuracy through a timed exercise, complete with performance analytics and random text generation."
-              technologies={['Javascript']} 
-              githubLink="https://github.com/brinzunza/Typing_test" 
-              liveDemoLink="https://justtyping.netlify.app" 
+              title="Salary Management System" 
+              description="Designed and implemented a simulation of a real-life salary management system."
+              technologies={['SQL', 'Databases', 'Python']} 
+              githubLink="https://github.com/brinzunza/salary-management" 
+              liveDemoLink="https://github.com/brinzunza/salary-management" 
             />
             <ProjectCard 
-              title="Image Classification" 
-              description="Developed an image classification application using machine learning techniques, enabling users to upload custom input and receive instant feedback on the classification results."
-              technologies={['Python']} 
-              githubLink="https://github.com/brinzunza/OCR" 
-              liveDemoLink="https://github.com/brinzunza/OCR/blob/main/ocr.py" 
-            />
-            <ProjectCard 
-              title="F1 Visualization Dashboard" 
+              title="Machine Learning From Scratch" 
               description="Created a dynamic dashboard for visualizing Formula 1 race data, allowing users to explore race statistics, driver performance, and team standings through interactive charts and graphs."
-              technologies={['Python', 'Flask']} 
-              githubLink="https://github.com/brinzunza" 
-              liveDemoLink="https://github.com/brinzunza" 
+              technologies={['Python', 'Machine Learning']} 
+              githubLink="https://github.com/brinzunza/machine-learning-from-scratch" 
+              liveDemoLink="https://github.com/brinzunza/machine-learning-from-scratch" 
             />
           </div>
         </section>
